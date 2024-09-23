1ca5f3e93b5847e92651c2c9c7a32b7958f94d13
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2024-09-23 16:30:19 +0300

    Add project files.

---
+++ OrchestrationFunctionApp/CallbackFunction.cs
@@ -0,0 +1,30 @@
+using System;
+using Azure.Messaging.ServiceBus;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace OrchestrationFunctionApp
+{
+    public class CallbackFunction
+    {
+        [FunctionName("CallbackFunction")]
+        public static async Task Run(
+        [ServiceBusTrigger("pipeline-event", Connection = "QueueConnectionString", IsSessionsEnabled = true)] ServiceBusReceivedMessage message, ILogger log)
+        {
+            try
+            {
+                log.LogInformation($"Received message with ApplicationId: {message.ApplicationProperties["SequenceId"]}");
+                string messageBody = System.Text.Encoding.UTF8.GetString(message.Body);
+
+                // Simulate processing
+                log.LogInformation($"Message body: {messageBody}");
+                await Task.Delay(1000);  // Simulate some processing work
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Failed to process message: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ OrchestrationFunctionApp/GatewayFunction.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
+
+namespace OrchestrationFunctionApp
+{
+    public static class GatewayFunction
+    {
+        [FunctionName("GatewayFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Gateway function has started...");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            // Initialize Service bus connection 
+            string connectionstring = Environment.GetEnvironmentVariable("QueueConnectionString");
+            ServiceBusClient serviceBusClient = new ServiceBusClient(connectionstring);
+
+            // Initialize a sender object with queue name
+            var pipelineEventQueueSender = serviceBusClient.CreateSender("pipeline-event");
+
+            // Create message for service bus
+            dynamic payload = JsonConvert.DeserializeObject(requestBody);
+            string sessionId = (string)payload.CorrelationId;
+            //string sessionId = Guid.NewGuid().ToString();
+
+            for (int i = 1; i < 10; i++)
+            {
+                // Send the Message 
+                ServiceBusMessage message = new ServiceBusMessage(requestBody);
+                message.CorrelationId = sessionId;
+                message.ApplicationProperties.Add("SequenceId", i);
+                await pipelineEventQueueSender.SendMessageAsync(message);
+            }
+
+            string responseMessage = "Sent with success.";
+            return new OkObjectResult(responseMessage);
+
+        }
+    }
+}
