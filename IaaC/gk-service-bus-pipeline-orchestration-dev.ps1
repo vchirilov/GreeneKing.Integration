2ebcd5edc2989d938904db4e215411f424bdfe6d
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2024-10-28 16:19:36 +0200

    No need these files

--- IaaC/gk-service-bus-pipeline-orchestration-dev.ps1
+++
@@ -1,3 +0,0 @@
-$rg = "z114-d-evnt"
-New-AzResourceGroup -Name $rg -Location northeurope -Force
-New-AzResourceGroupDeployment -Name "create-message-broker-01" -ResourceGroupName $rg -TemplateFile "gk-service-bus-pipeline-orchestration.json"
