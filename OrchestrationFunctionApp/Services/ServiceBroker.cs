eafbf1877841b26f2a533757e5fd317acd06f0a8
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2024-12-11 15:28:22 +0200

    Corrected the text

--- OrchestrationFunctionApp/Services/ServiceBroker.cs
+++ OrchestrationFunctionApp/Services/ServiceBroker.cs
@@ -25,7 +25,7 @@ namespace OrchestrationFunctionApp.Services
             _logger = logger;
             _serviceBusSettings = serviceBusSettings.Value;
 
-            _logger.LogInformation($"Queue defined in configuration is ${_serviceBusSettings.QueueName}");
+            _logger.LogInformation($"Queue defined in configuration is {_serviceBusSettings.QueueName}");
         }
 
         public async Task PublishAsync(object message)
