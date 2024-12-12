0945e6ed86daf641966cf2f9ef5f0fff5ab0853a
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2024-12-12 15:40:43 +0200

    Commented these functions as they aren't relevant.

--- OrchestrationFunctionApp/Functions/GatewayFunction.cs
+++ OrchestrationFunctionApp/Functions/GatewayFunction.cs
@@ -1,75 +1,75 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Azure.Messaging.ServiceBus;
-using OrchestrationFunctionApp.Models;
-using System.Linq;
-using Microsoft.Extensions.Options;
-using OrchestrationFunctionApp.Options;
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Extensions.Http;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.Extensions.Logging;
+//using Newtonsoft.Json;
+//using Azure.Messaging.ServiceBus;
+//using OrchestrationFunctionApp.Models;
+//using System.Linq;
+//using Microsoft.Extensions.Options;
+//using OrchestrationFunctionApp.Options;
 
-namespace OrchestrationFunctionApp.Functions
-{
-    public class GatewayFunction
-    {
-        private readonly ILogger<GatewayFunction> _logger;
-        private readonly ServiceBusSettings _serviceBusSettings;
+//namespace OrchestrationFunctionApp.Functions
+//{
+//    public class GatewayFunction
+//    {
+//        private readonly ILogger<GatewayFunction> _logger;
+//        private readonly ServiceBusSettings _serviceBusSettings;
 
-        public GatewayFunction(ILogger<GatewayFunction> logger, IOptions<ServiceBusSettings> serviceBusSettings)
-        {
-            _logger = logger;
-            _serviceBusSettings = serviceBusSettings.Value;
-        }
+//        public GatewayFunction(ILogger<GatewayFunction> logger, IOptions<ServiceBusSettings> serviceBusSettings)
+//        {
+//            _logger = logger;
+//            _serviceBusSettings = serviceBusSettings.Value;
+//        }
 
-        [FunctionName("gateway-function")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
-        {
-            try
-            {
-                _logger.LogInformation("Gateway function has started...");
+//        [FunctionName("gateway-function")]
+//        public async Task<IActionResult> Run(
+//            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+//        {
+//            try
+//            {
+//                _logger.LogInformation("Gateway function has started...");
 
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+//                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-                // Initialize queue sender            
-                ServiceBusClient serviceBusClient = new ServiceBusClient(_serviceBusSettings.ConnectionString);
-                var pipelineEventQueueSender = serviceBusClient.CreateSender(_serviceBusSettings.QueueName);
+//                // Initialize queue sender            
+//                ServiceBusClient serviceBusClient = new ServiceBusClient(_serviceBusSettings.ConnectionString);
+//                var pipelineEventQueueSender = serviceBusClient.CreateSender(_serviceBusSettings.QueueName);
 
-                // Deserialize payload into PipelineDescriptor
-                PipelineDescriptor pipelineDescriptor = JsonConvert.DeserializeObject<PipelineDescriptor>(requestBody);
+//                // Deserialize payload into PipelineDescriptor
+//                PipelineDescriptor pipelineDescriptor = JsonConvert.DeserializeObject<PipelineDescriptor>(requestBody);
 
-                var workflowsOrderedBy = pipelineDescriptor.Pipeline.OrderBy(x => x.OrderId);
+//                var workflowsOrderedBy = pipelineDescriptor.Pipeline.OrderBy(x => x.OrderId);
 
-                foreach (var workflow in workflowsOrderedBy)
-                {
-                    await SendMessage(pipelineEventQueueSender, workflow);
-                }
+//                foreach (var workflow in workflowsOrderedBy)
+//                {
+//                    await SendMessage(pipelineEventQueueSender, workflow);
+//                }
 
-                string responseMessage = "Sent with success.";
-                return new OkObjectResult(responseMessage);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"GatewayFunction failed to process message: {ex.Message}");
-                return new BadRequestObjectResult(ex);
-            }           
+//                string responseMessage = "Sent with success.";
+//                return new OkObjectResult(responseMessage);
+//            }
+//            catch (Exception ex)
+//            {
+//                _logger.LogError($"GatewayFunction failed to process message: {ex.Message}");
+//                return new BadRequestObjectResult(ex);
+//            }           
 
-        }
+//        }
 
-        private static async Task SendMessage(ServiceBusSender pipelineEventQueueSender, Workflow workflow)
-        {
-            var jsonWorkflow = JsonConvert.SerializeObject(workflow);
+//        private static async Task SendMessage(ServiceBusSender pipelineEventQueueSender, Workflow workflow)
+//        {
+//            var jsonWorkflow = JsonConvert.SerializeObject(workflow);
 
-            ServiceBusMessage message = new ServiceBusMessage(jsonWorkflow);
-            message.CorrelationId = "abcd";
-            message.ApplicationProperties.Add("SequenceId", 'a');
+//            ServiceBusMessage message = new ServiceBusMessage(jsonWorkflow);
+//            message.CorrelationId = "abcd";
+//            message.ApplicationProperties.Add("SequenceId", 'a');
 
-            await pipelineEventQueueSender.SendMessageAsync(message);
-        }
-    }
-}
+//            await pipelineEventQueueSender.SendMessageAsync(message);
+//        }
+//    }
+//}
