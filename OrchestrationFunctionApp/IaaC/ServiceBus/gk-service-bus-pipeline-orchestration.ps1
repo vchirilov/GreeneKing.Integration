1a1f858c3a009adc661601b2cf92d461ae1f617b
Veaceslav Chirilov <veaceslav.chirilov@amdaris.com>
2024-12-17 13:51:36 +0200

    Removed unnecessary files

--- OrchestrationFunctionApp/IaaC/ServiceBus/gk-service-bus-pipeline-orchestration.ps1
+++
@@ -1,3 +0,0 @@
-$rg = "z114-d-evnt"
-New-AzResourceGroup -Name $rg -Location westeurope -Force
-New-AzResourceGroupDeployment -Name "create-service-bus-01" -ResourceGroupName $rg -TemplateFile "gk-service-bus-pipeline-orchestration.json"
